2996a85a57d27ca0545860118c3a82deb73c90f5
Ysabel Garcia <ysabel.garcia@live.com>
2021-08-31 04:14:11 +0000

    adding a second route and template

--- express_server.js
+++ express_server.js
@@ -13,12 +13,18 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
