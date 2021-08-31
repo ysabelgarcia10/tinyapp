365569c11ef769c3655a0b06a859cc6d6fe25488
Ysabel Garcia <ysabel.garcia@live.com>
2021-08-31 05:19:37 +0000

    add a header partial to urls_index and urls_show. updated the longurl key in express_server.js

--- express_server.js
+++ express_server.js
@@ -23,7 +23,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
