7501b92a1ccfae30c87731020bbc224ce7d2b863
Ysabel Garcia <ysabel.garcia@live.com>
2021-08-31 05:54:15 +0000

    generated a random shortURL on express_server and downloaded body-parser middleware to handle POST requests.

--- express_server.js
+++ express_server.js
@@ -1,26 +1,42 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const uniqueURL = "";
+
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  uniqueURL = Math.random().toString(36).substr(2, 6);
+  return uniqueURL;
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+// app.get("/urls", (req, res) => {
+//   const templateVars = { urls: urlDatabase };
+//   res.render("urls_index", templateVars);
+// });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
